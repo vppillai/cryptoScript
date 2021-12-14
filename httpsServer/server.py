241c2f94fe05dbca2ccff57f521d133750024560
Vysakh P Pillai <3634378+vppillai@users.noreply.github.com>
2021-12-14 14:56:38 +0530

    Update server.py

--- httpsServer/server.py
+++ httpsServer/server.py
@@ -6,7 +6,7 @@ httpd = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket, certfile="ECC_prime256v1.cer", keyfile="ECC_prime256v1.key", server_side=True)
 
 #if Mutual auth is required
-#httpd.socket = ssl.wrap_socket(httpd.socket, certfile="ECC_prime256v1.cer", keyfile="ECC_prime256v1.key", server_side=True, cert_reqs=ssl.CERT_REQUIRE, ca_certs="./caCerts")
+#httpd.socket = ssl.wrap_socket(httpd.socket, certfile="ECC_prime256v1.cer", keyfile="ECC_prime256v1.key", server_side=True, cert_reqs=ssl.CERT_REQUIRED, ca_certs="./caCerts")
 
 print("Server running on https://0.0.0.0:" + str(port))
 
